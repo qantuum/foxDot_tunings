9cd36a9b9bb4a7e637643a5f095c5f3be86d4c88
qantuum <36848803+qantuum@users.noreply.github.com>
2019-01-14 14:11:42 +0100

    Update tunings.py

--- tunings.py
+++ tunings.py
@@ -5,14 +5,14 @@
 ################## Equal Temperament tunings (select a class) ##################
 
 class ET14:
-    tuning = PRange(15)*12/14,
-    chromatic = PRange(15),   
+    tuning = PRange(15)*12/14
+    chromatic = PRange(15)  
 
 class ET17:
-    tuning = PRange(18)*12/17,
-    chromatic = PRange(18),
-    otonal = P[0,3,5,8,10,12,14],
-    neutral = P[0,2,5,7,10,12,15],
+    tuning = PRange(18)*12/17
+    chromatic = PRange(18)
+    otonal = P[0,3,5,8,10,12,14]
+    neutral = P[0,2,5,7,10,12,15]
     
 Scale.default.set(ET17.otonal,tuning=ET17.tuning)
 
