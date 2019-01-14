d6aa374b00efa5cc18506f605f33ed87b2b798d9
qantuum <36848803+qantuum@users.noreply.github.com>
2019-01-14 13:36:39 +0100

    Update tunings.py

--- tunings.py
+++ tunings.py
@@ -1,15 +1,16 @@
-# Copy/Paste in FoxDot IDE
+################## Copy/Paste your method of choice in FoxDot IDE ##################
 
-# Equal Temperament tunings w/ scales
+################## Equal Temperament tunings w/ scales (3 steps) ##################
 
-# auto-done part
+# auto-done part - it just puts all info in memory
 # def tuning function
 def equal(temp):    
     return PRange(temp+1)*12/temp
 # def scales functions - Careful, when len(tuning) != 12, the scales pre-defined in the Scale class will not work properly
 def scales13() :
    return {
-     'major13':P[0,2,6,8,9,11,12]
+     'chromatic':PRange(14),
+     'major':P[0,2,6,8,9,11,12],
    }
 # var scales dictionary
 scales = {
@@ -21,13 +22,13 @@ temp = 13
 print(equal(temp))
 print(scales[temp]())
 
-# Set scale default - now it requires to know the scale name (see print output above)
-Scale.default.set(scales[temp]()['major13'], tuning=equal(temp))
+# Set scale default - now it requires to know the scale name (see print output above) - always defaults to chromatic
+Scale.default.set(scales[temp]()['chromatic'], tuning=equal(temp))
 
-# Equal temparent other than octave tunings w/ scales
+################## Equal temparent other than octave tunings w/ scales ##################
 
 Scale.default.set(Scale.major, tuning=Tuning.bohlen_pierce)
 
-# Just Intonation tunings w/ scales
+################## Just Intonation tunings w/ scales ##################
 
 Scale.default.set(Scale.major, tuning=Tuning.just)
