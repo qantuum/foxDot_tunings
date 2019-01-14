32eb449f8660aeac4170e20d06d9a23d91c7eec9
qantuum <36848803+qantuum@users.noreply.github.com>
2019-01-14 18:07:47 +0100

    Update tunings.py

--- tunings.py
+++ tunings.py
@@ -7,12 +7,20 @@
 class ET14:
     tuning = PRange(15)*12/14
     chromatic = PRange(15)
+    major = P[0,2,4,6,9,11,133]
 
 class ET17:
     tuning = PRange(18)*12/17
     chromatic = PRange(18)
     otonal = P[0,3,5,8,10,12,14]
     neutral = P[0,2,5,7,10,12,15]
+    
+Class ET18:
+    tuning = PRange(19)*12/18
+    chromatic = PRange(19)
+    minor = P[0,3,5,8,11,13,15]
+    major = P[0,3,6,8,11,14,17]
+
     
 Scale.default.set(ET17.otonal,tuning=ET17.tuning)
 
