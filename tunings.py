e3dd024fcef715c4d8d2f356336c7c534c9107d7
qantuum <36848803+qantuum@users.noreply.github.com>
2019-01-14 13:50:06 +0100

    Update tunings.py

--- tunings.py
+++ tunings.py
@@ -1,20 +1,29 @@
 ################## Copy/Paste your method of choice in FoxDot IDE ##################
 
-################## Equal Temperament tunings w/ scales (3 steps) ##################
+################## Find out about these scale names on https://en.xen.wiki/ ##################
 
-# auto-done part - it just puts all info in memory
+################## Equal Temperament tunings (3 steps) ##################
+
+# Careful, when len(tuning) != 12, the scales pre-defined in the Scale class will not work properly
+# auto-done part - it just puts info in memory
 # def tuning function
 def equal(temp):    
     return PRange(temp+1)*12/temp
-# def scales functions - Careful, when len(tuning) != 12, the scales pre-defined in the Scale class will not work properly
+# def scales functions
 def scales13() :
    return {
      'chromatic':PRange(14),
      'major':P[0,2,6,8,9,11,12],
    }
+def scales17() :
+    return {
+        'otonal':P[0,3,5,8,10,12,14],
+        'neutral':P[0,2,5,7,10,12,15],
+    }
 # var scales dictionary
 scales = {
-  13:scales13
+  13:scales13,
+  17:scales17
 }
 
 # input your temperament here :
