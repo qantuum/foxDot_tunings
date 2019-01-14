503eadd93f30bdf28f989199c13e985354aff62f
qantuum <36848803+qantuum@users.noreply.github.com>
2019-01-14 11:37:58 +0100

    Update tunings.py

--- tunings.py
+++ tunings.py
@@ -1,4 +1,20 @@
 # Copy/Paste in FoxDot IDE
 
+# Equal Temperament tunings w/ scales
+
+Tuning.ET5 = PRange(6)*12/5
+print(Tuning.ET5)
+
+Tuning.ET6 = PRange(7)*12/6
+print(Tuning.ET6)
+
 Tuning.ET10 = PRange(11)*12/10
 print(Tuning.ET10)
+
+# Equal temparent other than octave tunings w/ scales
+
+print(Tuning.bohlen_pierce)
+
+# Just Intonation tunings w/ scales
+
+print(Tuning.Just)
