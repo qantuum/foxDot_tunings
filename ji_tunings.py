20070c6b9f0a79e629bcb06e0eb7a1ff4743184b
qantuum <36848803+qantuum@users.noreply.github.com>
2019-01-25 11:20:22 +0100

    Update ji_tunings.py

--- ji_tunings.py
+++ ji_tunings.py
@@ -31,3 +31,5 @@ class Tunings:
 class Highschool1(Tunings):
   tuning = Tunings.toCent(P[1,21/20,9/8,6/5,5/4,4/3,7/5,3/2,8/5,5/3,7/4,15/8])
 
+class CarlosHarm(Tunings):
+  tuning = Tunings.toCent(P[1,17/16,9/8,19/16,5/4,21/16,11/8,3/2,13/8,27/16,7/4,15/8,2/1])
