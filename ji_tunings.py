24b79c5f4f9a2dec5128ad6b887ace15355d685d
qantuum <36848803+qantuum@users.noreply.github.com>
2019-01-25 11:10:00 +0100

    Update ji_tunings.py

--- ji_tunings.py
+++ ji_tunings.py
@@ -6,26 +6,28 @@ Scale.default.set(Scale.major, tuning=Tuning.just)
 # see JI collection @ https://en.xen.wiki/w/Gallery_of_12-tone_just_intonation_scales
 # this JI collection offers JI tunings of 12 notes that form an octave.
 
-# let's convert ratios to cents
-def toCent(exp):
-    import math
-    if (isinstance(exp, Pattern)):
-        for index in range(len(exp)):
-            exp[index] = 12*math.log(exp[index], 2)
-        return exp
-    else:
-        return exp
-
 # abstract class for distributing names method
 class Tunings:
-    def library(self):
+    @classmethod
+    def library(cls):
         lib = []
-        for items in (self.__class__.__dict__.items(), self.__dict__.items()):
+        for items in (cls.__class__.__dict__.items(), cls.__dict__.items()):
             lib.extend([(key, value) for key, value in items if isinstance(value, (Pattern))])
         return dict(lib)
-    def names(self):
-        return sorted(self.library().keys())
+    @classmethod
+    def names(cls):
+        return sorted(cls.library().keys())
+    # let's convert ratios to cents
+    @classmethod
+    def toCent(cls, exp):
+        import math
+        if (isinstance(exp, Pattern)):
+            for index in range(len(exp)):
+                exp[index] = 12*math.log(exp[index], 2)
+            return exp
+        else:
+            return exp
     
 class Highschool1(Tunings):
-  tuning = toCent(P[1,21/20,9/8,6/5,5/4,4/3,7/5,3/2,8/5,5/3,7/4,15/8])
+  tuning = Tunings.toCent(P[1,21/20,9/8,6/5,5/4,4/3,7/5,3/2,8/5,5/3,7/4,15/8])
 
