40ead913fad56698c8357b5d6fd67ac2df14167f
qantuum <36848803+qantuum@users.noreply.github.com>
2019-01-25 11:13:27 +0100

    Update et_tunings.py

--- et_tunings.py
+++ et_tunings.py
@@ -3,8 +3,20 @@
 ################## Equal Temperament tunings (select a class) ##################
 ################## "ET" or "Equal Temperament" can also be defined as "EDO" or "Equal Division of Octive"
 
-# global method to return cumulative sum
-def cumulative(exp):
+# abstract class for distributing names method
+class Tunings:
+    @classmethod
+    def library(cls):
+        lib = []
+        for items in (cls.__class__.__dict__.items(), cls.__dict__.items()):
+            lib.extend([(key, value) for key, value in items if isinstance(value, (Pattern))])
+        return dict(lib)
+    @classmethod
+    def names(cls):
+        return sorted(cls.library().keys())
+    # global method to return cumulative sum
+    @classmethod
+    def cumulative(cls, exp):
         if (isinstance(exp, Pattern)):
             for index in range(len(exp)):
                 if index != 0:
@@ -12,16 +24,6 @@ def cumulative(exp):
             return exp
         else:
             return exp
-
-# abstract class for distributing names method
-class Tunings:
-    def library(self):
-        lib = []
-        for items in (self.__class__.__dict__.items(), self.__dict__.items()):
-            lib.extend([(key, value) for key, value in items if isinstance(value, (Pattern))])
-        return dict(lib)
-    def names(self):
-        return sorted(self.library().keys())
 
 class ET10(Tunings):
     tuning = PRange(11)*12/10
@@ -31,18 +33,18 @@ class ET10(Tunings):
 class ET11(Tunings):
     tuning = PRange(12)*12/11
     chromatic = PRange(12)
-    machine5 = cumulative(P[0,2,2,2,2])
-    machine6 = cumulative(P[0,2,2,2,2,2])
-    orgone7 = cumulative(P[0,1,2,1,2,1,2])
-    mavilaC = cumulative(P[0,1,1,1,3,1,1])
-    mavilaD = cumulative(P[0,1,1,3,1,1,3])
-    mavilaE = cumulative(P[0,1,3,1,1,3,1])
-    mavilaF = cumulative(P[0,3,1,1,3,1,1])
-    mavilaG = cumulative(P[0,1,1,3,1,1,1])
-    mavilaA = cumulative(P[0,1,3,1,1,1,3])
-    mavilaB = cumulative(P[0,3,1,1,1,3,1])
-    classicPenta = cumulative(P[0,1,4,1,4])
-    swoon = cumulative(P[0,2,3,1,3])
+    machine5 = Tunings.cumulative(P[0,2,2,2,2])
+    machine6 = Tunings.cumulative(P[0,2,2,2,2,2])
+    orgone7 = Tunings.cumulative(P[0,1,2,1,2,1,2])
+    mavilaC = Tunings.cumulative(P[0,1,1,1,3,1,1])
+    mavilaD = Tunings.cumulative(P[0,1,1,3,1,1,3])
+    mavilaE = Tunings.cumulative(P[0,1,3,1,1,3,1])
+    mavilaF = Tunings.cumulative(P[0,3,1,1,3,1,1])
+    mavilaG = Tunings.cumulative(P[0,1,1,3,1,1,1])
+    mavilaA = Tunings.cumulative(P[0,1,3,1,1,1,3])
+    mavilaB = Tunings.cumulative(P[0,3,1,1,1,3,1])
+    classicPenta = Tunings.cumulative(P[0,1,4,1,4])
+    swoon = Tunings.cumulative(P[0,2,3,1,3])
 
 
 class ET14(Tunings):
@@ -65,25 +67,25 @@ class ET18(Tunings):
 class ET20(Tunings):
     tuning = PRange(21)*12/20
     chromatic = PRange(21)
-    blackwood10Major = cumulative(P[0,3,1,3,1,3,1,3,1,3])
-    blackwood10Minor = cumulative(P[0,1,3,1,3,1,3,1,3,1])
-    balzano9 = cumulative(P[0,2,3,2,2,2,3,2,2])
-    balzano11 = cumulative(P[0,2,2,2,2,1,2,2,2,2,2])
-    balzano9Inverse = cumulative(P[0,2,2,2,3,2,2,2,3])
-    balzano11Inverse = cumulative(P[0,1,2,2,2,2,2,1,2,2,2])
-    octatonic = cumulative(P[0,2,3,2,3,2,3,2])
-    diminished = cumulative(P[0,3,2,3,2,3,2,3])
-    major = cumulative(P[0,4,3,1,4,3,4])
-    minor = cumulative(P[0,4,1,3,4,1,4])
-    chromatic12 = cumulative(P[0,2,2,1,2,1,2,2,1,2,2,2])
-    zweifelMajor = cumulative(P[0,2,2,2,2,1,2,2,2,2,1])
-    zweifelMinor = cumulative(P[0,2,1,2,2,2,2,2,1,2,2])
-    majorQuasi = cumulative(P[0,3,3,3,3,3,3])
-    minorQuasi = cumulative(P[0,3,2,3,3,3,3])
-    rotenberg = cumulative(P[0,3,2,2,2,2,3,2,2])
-    stearnsMajor = cumulative(P[0,3,4,1,4,3,3])
-    pentatonic = cumulative(P[0,7,2,7,2])
-    antiDiatonic = cumulative(P[0,5,2,2,5,2,2])
+    blackwood10Major = Tunings.cumulative(P[0,3,1,3,1,3,1,3,1,3])
+    blackwood10Minor = Tunings.cumulative(P[0,1,3,1,3,1,3,1,3,1])
+    balzano9 = Tunings.cumulative(P[0,2,3,2,2,2,3,2,2])
+    balzano11 = Tunings.cumulative(P[0,2,2,2,2,1,2,2,2,2,2])
+    balzano9Inverse = Tunings.cumulative(P[0,2,2,2,3,2,2,2,3])
+    balzano11Inverse = Tunings.cumulative(P[0,1,2,2,2,2,2,1,2,2,2])
+    octatonic = Tunings.cumulative(P[0,2,3,2,3,2,3,2])
+    diminished = Tunings.cumulative(P[0,3,2,3,2,3,2,3])
+    major = Tunings.cumulative(P[0,4,3,1,4,3,4])
+    minor = Tunings.cumulative(P[0,4,1,3,4,1,4])
+    chromatic12 = Tunings.cumulative(P[0,2,2,1,2,1,2,2,1,2,2,2])
+    zweifelMajor = Tunings.cumulative(P[0,2,2,2,2,1,2,2,2,2,1])
+    zweifelMinor = Tunings.cumulative(P[0,2,1,2,2,2,2,2,1,2,2])
+    majorQuasi = Tunings.cumulative(P[0,3,3,3,3,3,3])
+    minorQuasi = Tunings.cumulative(P[0,3,2,3,3,3,3])
+    rotenberg = Tunings.cumulative(P[0,3,2,2,2,2,3,2,2])
+    stearnsMajor = Tunings.cumulative(P[0,3,4,1,4,3,3])
+    pentatonic = Tunings.cumulative(P[0,7,2,7,2])
+    antiDiatonic = Tunings.cumulative(P[0,5,2,2,5,2,2])
     
 class ET22(Tunings):
     tuning = PRange(23)*12/22
@@ -93,16 +95,16 @@ class ET22(Tunings):
 class ET23(Tunings):
     tuning = PRange(24)*12/22
     chromatic = PRange(24)
-    keter = cumulative(P[0,2,2,2,3,2,2,3,2,2])
-    chesed = cumulative(P[0,2,2,3,2,2,3,2,2,3])
-    netzach = cumulative(P[0,2,3,2,2,3,2,2,3,2])
-    malkuth = cumulative(P[0,3,2,2,3,2,2,3,2,2])
-    binah = cumulative(P[0,2,2,3,2,2,3,2,2,2])
-    tiferet = cumulative(P[0,2,3,2,2,3,2,2,2,3])
-    yesod = cumulative(P[0,3,2,2,3,2,2,2,3,2])
-    chokmah = cumulative(P[0,2,2,3,2,2,2,3,2,2])
-    gevurah = cumulative(P[0,2,3,2,2,2,3,2,2,3])
-    hod = cumulative(P[0,3,2,2,2,3,2,2,3,2])
+    keter = Tunings.cumulative(P[0,2,2,2,3,2,2,3,2,2])
+    chesed = Tunings.cumulative(P[0,2,2,3,2,2,3,2,2,3])
+    netzach = Tunings.cumulative(P[0,2,3,2,2,3,2,2,3,2])
+    malkuth = Tunings.cumulative(P[0,3,2,2,3,2,2,3,2,2])
+    binah = Tunings.cumulative(P[0,2,2,3,2,2,3,2,2,2])
+    tiferet = Tunings.cumulative(P[0,2,3,2,2,3,2,2,2,3])
+    yesod = Tunings.cumulative(P[0,3,2,2,3,2,2,2,3,2])
+    chokmah = Tunings.cumulative(P[0,2,2,3,2,2,2,3,2,2])
+    gevurah = Tunings.cumulative(P[0,2,3,2,2,2,3,2,2,3])
+    hod = Tunings.cumulative(P[0,3,2,2,2,3,2,2,3,2])
 
     
 class ET36(Tunings):
@@ -111,7 +113,7 @@ class ET36(Tunings):
 
 # application example
 
-print(ET17().names())
+print(ET17.names())
 
 Scale.default.set(ET17.otonal,tuning=ET17.tuning)
 
